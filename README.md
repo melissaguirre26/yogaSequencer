6504bd42ace6d4233e94a4697bc24ff67aaffc9b
Melissa Aguirre <aguirrepgmelissa@gmail.com>
2018-08-04 08:45:56 -0700

    worked on a11y

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 This app is an app to create yoga sequences based on the yoga types the user selects
 On the main screen - the user is asked - what are you in the mood for? Ex. back-bends, core strengthening postures, etc.  Once the user selects 3 or more types - the user will click the om button and random postures will be displayed for the user to incorporate into their yoga practice.  The postures can be rearrange
+
+work on repl and paste over visual studio files then do git
+git add .
+git commit -m "write message"
+git push
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let btn = document.getElementById('btn');
+
 
 function reqListener() {
 	let data = JSON.parse(this.responseText);
 	let ul = document.getElementById('yogaUl');
-	ul.innerHTML = '';
 	let tracker = [];
 	// for (let i = 0; i < data.items.length; i++){
 	let length = data.items.length < 5 ? data.items.length - 1 : 4;
@@ -53,15 +52,16 @@ function reqListener() {
 
 	// we were running this code whether the pose was duplicated or not.
 	// we just need to seperate it out from the loop
-tracker.forEach(pose => {
+	tracker.forEach(pose => {
 		let name = `${pose.sanskrit_name} - ${pose.english_name}`;
 		let imgUrl = pose.img_url;
-		let template = `<li><div><div>${name}</div><img src="${imgUrl}"/></div></div></li>`;
+		let template = `<li><div><div class="nameFont">${name}</div><img id="image" class="box" src="${imgUrl}" alt="${name}" /></div></div></li>`;
+
 		ul.insertAdjacentHTML('beforeend', template)
 	});
 }
 
-	
+
 function getYogaPoses(yogaType) {
 	var oReq = new XMLHttpRequest();
 	oReq.addEventListener('load', reqListener);
@@ -73,20 +73,107 @@ function getYogaPoses(yogaType) {
 	oReq.send();
 }
 
-btn.addEventListener('click', function() {
-	let yogaType = document.getElementById('yogatypes').value;
 
-	getYogaPoses(yogaType);
-});
+
 
 function getRandomPose(posesArr) {
 	var num = Math.floor(Math.random() * posesArr.length);
 	return posesArr[num];
 }
-let yogaType = document.getElementById('yogatypes').value;
-document.getElementById('yogatypes').multiple = true;
 
-if (yogaType.selected == true){
-  alert(yogaType);
+
+
+//to create slideshow
+let slideShowPoses = [];
+let img_Index = 0;
+
+function showSlides() {
+	let imgLI = document.getElementsByTagName("LI");
+	for (i = 0; i < imgLI.length; i++) {
+		imgLI[i].style.display = "none";
+	}
+	img_Index++;
+
+	if (img_Index > imgLI.length) {
+		renderLast()
+	} else {
+		imgLI[img_Index - 1].style.display = "block";
+		setTimeout(showSlides, 30000)
+	}
+}//end of function showSlides
+
+
+function handleStartClicked() {
+	$('#namaste').click(() => {
+		renderChooseCategories();
+	});
+}
+
+function renderChooseCategories() {
+
+	let template = `<h2>What are you in the mood for?</h2>
+      <p>Select up to 3 yoga types below and hit the OM bottom</p>
+   <div id="yoga-info">
+    <select id="yogatypes" multiple size=12>
+  <option label="Core Yoga Poses">Core Yoga Poses</option>
+    <option label="Seated Yoga Poses">Seated Yoga Poses</option>
+    <option label="Strengthening">Strengthening</option>
+     <option label="Chest Opening">Chest Opening</option>
+    <option label="Yoga Backbends">Yoga Backbends</option>
+    <option label="Forward Bend Yoga Poses">Forward Bend Yoga Poses</option>
+     <option label="Hip Opening Yoga Poses">Hip Opening Yoga Poses</option>
+    <option label="Standing Yoga Poses">Standing Yoga Poses</option>
+     <option label="Restorative Yoga Poses">Restorative Yoga Poses</option>
+    <option label="Arm Balance Yoga Poses">Arm Balance Yoga Poses</option>
+     <option label="Balancing Yoga Poses">Balancing Yoga Poses</option>
+    <option label="Inversion Yoga Poses">Inversion Yoga Poses</option>
+</select>
+<button id="btnOM"></button>
+
+ </div>
+
+`
+	$('.container').html(template);
+	let btnOM = document.getElementById('btnOM');
+	btnOM.addEventListener('click', function () {
+		let yogaType = $('#yogatypes').val();
+		console.log(yogaType);
+		renderImgSS();
+		yogaType.forEach(function(type){
+	
+			getYogaPoses(type);
+		});
+	});
+
+
+
+}//end of renderChooseCategories
+
+function renderImgSS() {
+	let template = `<p>Follow the sequence below and hold each pose for at least 30 seconds</p>
+<ul id="yogaUl">
+</ul>
+<button id="btnSequence"></button>`
+	$('.container').html(template);
+	//add event listener to button start sequence
+	let btnSequence = document.getElementById("btnSequence");
+	btnSequence.addEventListener("click", showSlides);
 
 }
+
+function renderLast() {
+	let template = `<p>Congrats! You finished your yoga sequence!</p>
+
+	<button id="newSS"></button>`
+	$('.container').html(template);
+	//add event listener to button start sequence
+	let newSS = document.getElementById("newSS");
+	newSS.addEventListener("click", renderChooseCategories);
+
+}
+function runYogaSequence() {
+	handleStartClicked();
+
+}
+
+$(runYogaSequence);
